a51e52a3f10056606715ba8ebf4ce36366199022
HungDangQuang <hungdangkg@gmail.com>
2025-03-05 13:50:13 +0700

    first commit

---
+++ monex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  monex
+//
+//  Created by HUNG DANG on 5/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
